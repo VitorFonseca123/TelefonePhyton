b7aa3beac10f0fc71870bd8e45934b714a9d4709
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2022-05-26 18:34:52 -0300

    v1

---
+++ NumeroTelefone.py
@@ -0,0 +1,37 @@
+def isPhoneNumber(text):    
+    if len(text) != 9:
+        return False
+    for i in range(0, 4):
+        if not text[i].isdecimal():
+            return False
+        if text[4] != '-':
+            return False
+    for i in range(5, 9):
+        if not text[i].isdecimal():
+            return False
+    return True
+def isCellNumber(text):
+    if len(text) != 10:
+        return False
+    for i in range(0, 5):
+        if not text[i].isdecimal():
+            return False
+        if text[5] != '-':
+            return False
+    for i in range(6, 10):
+        if not text[i].isdecimal():
+            return False
+    return True
+def cellORphone(number):
+    print(number[0])
+    if number[0]!=9:
+        print(number, ' é um número de telefone:')
+        print(isPhoneNumber(number))   
+    elif number[0] == 9:
+        print(number, ' é um número de celular:')
+        print(isCellNumber(number))
+number = input('Digite o número de telefone: ')
+cellORphone(number)
+
+# 2749-8937
+#98143-3618
